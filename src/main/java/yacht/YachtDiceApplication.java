b3cb448ffa2bcdb24a96cf34d2d4cff8aa59872f
Unho Yeo <yuh001205@gmail.com>
2025-11-15 22:10:12 +0900

    init commit

---
+++ src/main/java/yacht/YachtDiceApplication.java
@@ -0,0 +1,13 @@
+package yacht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YachtDiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YachtDiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yacht/YachtDiceApplicationTests.java
@@ -0,0 +1,13 @@
+package yacht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YachtDiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
